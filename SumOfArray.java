c8a8453ad6d53501ac59ca4a67ff0445e720c724
reddivarinandana <nadanareddy1122@gmail.com>
2023-09-04 02:33:13 +0530

    Sum Of All Elements In Array

---
+++ SumOfArray.java
@@ -0,0 +1,15 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        Sum();
+    }
+    public static void Sum() {
+        int[] arr={1,2,3,4,5};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
+
+
